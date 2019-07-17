bc83d7fd8cabda46aece0d51f6737d8bbae2e9ea
Brianna Kelsey <briannagkelsey@gmail.com>
2019-07-16 20:05:44 -0400

    Deals with merge conflicts

--- routes/api/index.js
+++ routes/api/index.js
@@ -12,4 +12,12 @@ router.use("/barber", barberRoutes);
 // // Book routes
 // router.use("/books", bookRoutes);
 
-module.exports = router;
+// // Google Routes
+// router.use("/google", googleRoutes);
+
+// // For anything else, render the html page
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+// });
+
+// module.exports = router;
