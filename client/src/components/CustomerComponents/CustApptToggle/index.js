5b8b5e0b191420230b33da619fea152fa9e9301d
aevers3 <aevers3@users.noreply.github.com>
2019-07-17 13:07:38 -0400

    more styling - upcoming appointment box and cards

--- client/src/components/CustomerComponents/CustApptToggle/index.js
+++ client/src/components/CustomerComponents/CustApptToggle/index.js
@@ -6,15 +6,15 @@ const CustApptToggle = props => {
     if (props.upcomingApptsView) {
         return (
             <>
-                <span className='not-checked p-2 pt-3'>Upcoming Appointments</span>
-                <button onClick={props.handleToggleView} className='toggle-btn'>Past Appointments</button>
+                <span className='not-checked p-2 pt-3 text-white'>Upcoming Appointments</span>
+                <button onClick={props.handleToggleView} className='toggle-btn btn-light rounded-pill'>Past Appointments</button>
             </>
         )
     } else {
         return (
             <>
-                <button onClick={props.handleToggleView} className='toggle-btn'>Upcoming Appointments</button>
-                <span className='not-checked p-2 pt-3'>Past Appointments</span>
+                <button onClick={props.handleToggleView} className='toggle-btn btn-light rounded-pill'>Upcoming Appointments</button>
+                <span className='not-checked p-2 pt-3 text-white'>Past Appointments</span>
             </>
         )
     }
