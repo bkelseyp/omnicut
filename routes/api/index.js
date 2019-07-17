68c6565c808e488bb246e7e89666d589e8dee0b6
Brianna Kelsey <46538102+bkelseyp@users.noreply.github.com>
2019-07-16 21:35:45 -0400

    updated index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,23 +1,14 @@
-const path = require('path');
+const path = require("path");
 const router = require("express").Router();
 const appointmentRoutes = require("./appointment");
 const availabilityRoutes = require("./availability");
 const barberRoutes = require("./barber");
-
 // routes
 router.use("/appointment", appointmentRoutes);
 router.use("/availability", availabilityRoutes);
 router.use("/barber", barberRoutes);
-
-// // Book routes
-// router.use("/books", bookRoutes);
-
-// // Google Routes
-// router.use("/google", googleRoutes);
-
-// // For anything else, render the html page
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-// });
-
-// module.exports = router;
+// For anything else, render the html page
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+module.exports = router;
