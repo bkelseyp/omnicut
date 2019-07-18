afdedf66a897477b411cb798fbe19c90aa9f81b7
aevers3 <aevers3@users.noreply.github.com>
2019-07-18 11:24:19 -0400

    vendor page styling tweaks

--- client/src/components/VendorComponents/VendorAvailabilityToggle/index.js
+++ client/src/components/VendorComponents/VendorAvailabilityToggle/index.js
@@ -6,15 +6,15 @@ const VendorAvailabilityToggle = props => {
     if (props.setAvailabilityView) {
         return (
             <div className='row d-flex justify-content-center mt-5'>
-                <span id='notAvailToggle' className='not-checked p-2 pt-3 col-sm-4 text-center not-checked-custom text-warning'>Set Your Availability</span>
-                <button onClick={props.handleAvailabilityToggle} className='toggle-btn availToggle col-sm-4 text-center btn-lg btn-warning rounded-pill'>View Your Availability</button>
+                <span id='notAvailToggle' className='not-checked p-2 pt-3 col-sm-4 text-center not-checked-custom'>Set Your Availability</span>
+                <button onClick={props.handleAvailabilityToggle} className='toggle-btn availToggle col-sm-4 text-center text-white btn-lg rounded-pill'>View Your Availability</button>
             </div>
         )
     } else {
         return (
             <div className='row d-flex justify-content-center mt-5'>            
-                <button onClick={props.handleAvailabilityToggle} className='toggle-btn availToggle col-sm-4 text-center btn-lg btn-primary rounded-pill'>Set Your Availability</button>
-                <span id='notAvailToggle' className='not-checked p-2 pt-3 col-sm-4 text-center not-checked-custom text-primary'>View Your Availability</span>
+                <button onClick={props.handleAvailabilityToggle} className='toggle-btn availToggle col-sm-4 text-center btn-lg text-white rounded-pill'>Set Your Availability</button>
+                <span id='notAvailToggle' className='not-checked p-2 pt-3 col-sm-4 text-center not-checked-custom'>View Your Availability</span>
             </div>
         )
     }
